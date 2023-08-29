b296f0795fd27b11bc1e53d3c28b3a25e906a165
ThomasSmidt <smidt592@gmail.com>
2023-08-29 11:19:59 +0200

    Added IsCompatibleDoctor

--- OOPBedømmelsesCase/Assignment.cs
+++ OOPBedømmelsesCase/Assignment.cs
@@ -15,18 +15,38 @@ namespace OOPBedømmelsesCase
         {
             Patient = patient;
             Doctor = doctor;
+
             if(doctor.Title == DoctorTypes.Onkolog && Assignments.Any(x => x.Patient == patient && x.Doctor.Title == DoctorTypes.Kirurg) 
                 || doctor.Title == DoctorTypes.Kirurg && Assignments.Any(x => x.Patient == patient && x.Doctor.Title == DoctorTypes.Onkolog))
             {
                 Console.WriteLine("Patienter kan ikke tildeles både en kirurg og en onkolog som læge.");
                 throw new Exception("Patienter kan ikke tildeles både en kirurg og en onkolog som læge.");
             }
+
             if (Assignments.Where(assignment => assignment.Doctor.Title == doctor.Title).Count() >= 3)
             {
                 Console.WriteLine("En læge må højst være tildelt 3 patienter.");
                 throw new Exception("En læge må højst være tildelt 3 patienter.");
             }
-            Assignments.Add(this);
+
+            if (!Assignments.Any(x => x.Doctor.Title == doctor.Title && x.Patient.FullName == patient.FullName))
+            {
+                Assignments.Add(this);
+            } else
+            {
+                Console.WriteLine("\nPatienten er allerede tildelt denne læge");
+            }
+        }
+
+        private bool IsCompatibleDoctor(Assignment assign)
+        {
+            if(assign.Doctor.Title == DoctorTypes.Onkolog && Assignments.Any(x => x.Patient == assign.Patient && x.Doctor.Title == DoctorTypes.Kirurg) 
+                || assign.Doctor.Title == DoctorTypes.Kirurg && Assignments.Any(x => x.Patient == assign.Patient && x.Doctor.Title == DoctorTypes.Onkolog))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
